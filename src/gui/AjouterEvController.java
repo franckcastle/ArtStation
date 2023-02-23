c19438e9acaeca683b2a0a8d6f39c7533e8723ba
wajdiouaili <87805679+franckcastle@users.noreply.github.com>
2023-02-23 15:01:41 +0100

    interfacesAjouter

--- src/gui/AjouterEvController.java
+++ src/gui/AjouterEvController.java
@@ -4,11 +4,15 @@ import entities.Evenement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import services.EvenementService;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class AjouterEvController implements Initializable {
@@ -24,6 +28,14 @@ public class AjouterEvController implements Initializable {
     public TextField localisationField;
     @FXML
     public TextField nbPlaceField;
+    @FXML
+    private DatePicker dateDebutField;
+
+    @FXML
+    private DatePicker dateFinFiel;
+    @FXML
+    private TextField prixField;
+
     EvenementService es = new EvenementService();
 
     @Override
@@ -38,10 +50,30 @@ public class AjouterEvController implements Initializable {
            e.setDescription(descriptionField.getText());
            e.setLocalisation(localisationField.getText());
            e.setNbPlace(Integer.parseInt(nbPlaceField.getText()));
+           //e.setDateDebut(dateDebutField.getValue().format());
+            LocalDate localDate = dateDebutField.getValue();
+            Date newDate = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            e.setDateDebut(newDate);
+            LocalDate localDate1 = dateFinFiel.getValue();
+            Date newDate1 = Date.from(localDate1.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            e.setDateFin(newDate1);
+            e.setPrix(Float.valueOf(prixField.getText()));
             try {
                 es.ajouterEv(e);
+                reset();
             } catch (Exception ex) {
                 System.out.println("error" + ex.getMessage());
             }
+
         }
+    private void reset() {
+        evaluationIField.setText("");
+        titreField.setText("");
+        descriptionField.setText("");
+        localisationField.setText("");
+        nbPlaceField.setText("");
+        //dateDebutField.setAccessibleText("");
+        //dateFinFiel.setAccessibleText("");
+        prixField.setText("");
+    }
 }
