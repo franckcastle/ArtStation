4d5c51f201a45252e773d00dd93d11018f2c67fe
Skander <medskander.gharbi@esprit.tn>
2023-03-03 00:56:56 +0100

    Upgrade structure +api

--- src/services/SendSms.java
+++ src/services/SendSms.java
@@ -1,20 +1,20 @@
-package test;
+package services;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-public class sms {
+public class SendSms {
         // Find your Account Sid and Token at twilio.com/user/account
         public static final String ACCOUNT_SID = "ACe09d66a6c3389c629930cfc1dccbd6c6";
-        public static final String AUTH_TOKEN = "111e4d93728f446914b695b09dc15c68";
+        public static final String AUTH_TOKEN = "669f57565a2d594e0fe736f0f03ac5b6";
 
-        public static void main(String[] args) {
+        public static void SendSms(String numero,String s) {
             Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-            Message message = Message.creator(new PhoneNumber("+21627005915"),
+            Message message = Message.creator(new PhoneNumber(numero),
                     new PhoneNumber("+13155993133"),
-                    "test").create();
+                    s).create();
 
-            System.out.println(message.getSid());
+
     }
 }
--- src/test/TestReclam.java
+++ src/test/TestReclam.java
@@ -1,15 +1,11 @@
 package test;
 
-import entities.Reclamation;
-import services.ReclamService;
+import services.SendSms;
 
 import java.sql.SQLException;
 
 public class TestReclam {
     public static void main(String[] args) throws SQLException {
-        Reclamation reclamation = new Reclamation(21,"skander","plainte");
-        ReclamService rs = new ReclamService();
-        rs.supprimer(reclamation);
     }
 
 }
